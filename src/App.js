e2f64f76baefee5ffbc63d6ed482762879893485
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-17 00:46:23 +0100

    Update url path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
       <section className="content">
         <Routes>
-          <Route path="/bookstore" element={<Books />} />
+          <Route path="/" element={<Books />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
