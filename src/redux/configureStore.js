428d28f7bb9d0edc1eb892b0b822e7a86fbdff11
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-20 13:22:58 +0100

    Fix linter errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import booksReducer from "./books/books";
-import categoryReducer from "./categories/categories";
+import booksReducer from './books/books';
+import categoryReducer from './categories/categories';
 
 const store = configureStore({
   reducer: {
