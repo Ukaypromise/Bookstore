d966415cb364bb5e91492240e2882992887f78c8
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-20 12:15:56 +0100

    Complete setup for redux

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,12 +1,20 @@
-const ADD_BOOK = 'bookStore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const ADD_BOOK = 'src/redux/books/ADD_BOOK';
+const REMOVE_BOOK = 'src/redux/books/REMOVE_BOOK';
 const initialState = [];
 
 // Reducer
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.payload];
+      return [
+        ...state,
+        {
+          title: action.title,
+          author: action.author,
+          category: action.category,
+          id: action.id,
+        },
+      ];
 
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
@@ -17,9 +25,17 @@ const booksReducer = (state = initialState, action) => {
 };
 
 // Action Creator
-export const addBook = (payload) => ({
+export const addBook = (
+  title = "Understanding Redux",
+  author = "Promise",
+  category = "Action",
+  id
+) => ({
   type: ADD_BOOK,
-  payload,
+  title,
+  author,
+  category,
+  id,
 });
 
 export const removeBook = (id) => ({
@@ -27,4 +43,5 @@ export const removeBook = (id) => ({
   id,
 });
 
+export { addBook, removeBook };
 export default booksReducer;
